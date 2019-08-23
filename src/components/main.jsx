0276eeb6efadf149436d7c195a21583ead662a28
B.Forte <b.forte@addermobile.com>
2019-08-23 13:45:20 -0400

    remove unused library

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,6 @@
 import React from "react";
  
-// import PageWithScene from "./pageWithScene" 
-// Example from babylon docs requires TextScript AND breaks anyway. https://doc.babylonjs.com/resources/babylonjs_and_reactjs
-import DefaultPlayground from './defaultPlayground'
-import WithModel from './WithModel'
+ 
   class Main extends React.Component {
 
 			constructor(props){
@@ -19,7 +16,7 @@ import WithModel from './WithModel'
 						
 						<div>
 							<div>Main</div>
-                            <WithModel/>
+                            
 						</div>
 															
 					);
