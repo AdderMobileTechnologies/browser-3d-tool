7643ffb979be6b89e19152024859a0416764e2d9
B.Forte <b.forte@addermobile.com>
2019-08-27 13:57:34 -0400

    clean up

--- src/models/model.js
+++ src/models/model.js
@@ -4,8 +4,6 @@ import BABYLON from 'babylonjs'; // Mesh, Vector3, Quaternion
 class AdderModel {
     constructor(modelFile = null, parentMesh = null, arrayOfMeshWrappers = null, position = null, rotation = null) {
 
-
-
         if (modelFile === "" || typeof modelFile !== "string") {
             throw new Error("AdderModel.getModelFile():  Constructor was sent an unspecified modelFile (Requires a string for file name.");
         }
@@ -13,15 +11,11 @@ class AdderModel {
         if (position === null || !(position instanceof BABYLON.Vector3)) {
             // throw new Error("AdderModel.Constructor(): Constructor called with unspecified position (Requires: BABYLON.Vector3 )")
             position = new BABYLON.Vector3(0, 0, 0);
-        } else {
-            position = position;// = new BABYLON.Vector3(0, 0, 0);
-        }
+        }  
         if (!(rotation instanceof BABYLON.Quaternion)) {
             //  throw new Error("AdderModel.Constructor(): Constructor called with unspecified rotation (Requires: BABYLON.Quaternion ")
             rotation = new BABYLON.Quaternion(0, 0, 0);
-        } else {
-
-        }
+        } 
 
         if (parentMesh === null || !(parentMesh instanceof BABYLON.Mesh)) {
             throw new Error("AdderModel.Constructor(): Constructor called with unspecified _newMeshParent (Requires: BABYLON.Mesh ")
@@ -58,27 +52,13 @@ class AdderModel {
             _parentMesh.setPositionWithLocalVector(positionVector3)
         }
       
-        // adderModel.setMeshWrappers(arrayOfMeshWrappers)
         this.setMeshWrappers = (newArrayOfMeshWrappers) => {
-            //check to make sure correct object HERE ? 
-            console.log("check the type of the array getting sent in to AdderModel as Array of Mesh Wrappers.....")
-            console.log("SETTING meshWrappers for : ", this.getModelFile())
-            console.log(typeof newArrayOfMeshWrappers);
             _arrayOfMeshWrappers = newArrayOfMeshWrappers
         }
         this.getMeshWrappers = () => {
             return _arrayOfMeshWrappers;
         }
 
-
-
-
-
-
-
     }// end constructor 
-
-
-
 }
 export default AdderModel;
