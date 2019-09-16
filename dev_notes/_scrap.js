6e5b0678d3628b0af0770326c7ad540db22396ad
B.Forte <b.forte@addermobile.com>
2019-09-16 09:40:58 -0400

    any changes from last week before starting this week.

--- dev_notes/_scrap.js
+++ dev_notes/_scrap.js
@@ -1,23 +1,7 @@
-save_UIAction() {
-  let action_object = this.state.actions;
-  action_object.action = "save_UIAction";
-  action_object.to = "change to";
-  action_object.from = "change from";
-  action_object.other =[];
-  const new_actions_array = this.state.actions.slice();
-  new_actions_array.push(action_object); // Push the object
-  this.setState(
-    prevState => ({
-      ...prevState,
-      actions: new_actions_array
-    }),
-    () => {
-  
-      let old_actions = JSON.parse(localStorage.getItem("actions_array")) || [];
-      let new_action = this.state.actions;
-      //push to local storage:
-      old_actions.push(new_action);
-      localStorage.setItem("actions_array", JSON.stringify(old_actions));
-    }
-  );
-}
+const array_texture_image_models = scope.state.texture_images.slice();
+console.log("texture_image_model:", texture_image_model);
+array_texture_image_models.push(texture_image_model);
+scope.setState(prevState => ({
+  ...prevState,
+  texture_images: array_texture_image_models
+}));
