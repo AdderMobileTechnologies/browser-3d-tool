6e5543985df4b903ebff4a4238fadfd8872f30d0
B.Forte <b.forte@addermobile.com>
2019-10-31 12:33:55 -0400

    rename unused directories.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //TEST COMMENT
-
+/*
 router.use('/api', require('./api'));
 router.use('/ext', require('./ext'));
 router.use('/auth', require('./auth/auth'));
 router.use('/v2', require('./v2'));
+*/
 module.exports = router;
