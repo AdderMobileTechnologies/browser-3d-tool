14aba222e52450c9885414b758600a88283e941f
B.Forte <b.forte@addermobile.com>
2019-11-20 15:22:09 -0500

    changes made to remove errors in the js console.

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import "babylonjs-loaders";
 import Login from "./authentication/login.jsx";
 
 //https://doc.babylonjs.com/features/npm_support#available-packages
-
+//maxWidth="xl"
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="xl"></Container>
+      <Container maxWidth={"xl"} children="none"></Container>
     </div>
   );
 }
